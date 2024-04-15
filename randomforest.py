db55eee6f9dc0ef5e5bd41a17b79aa68f9487969
Ida Bundgaard <idbun20@student.sdu.dk>
2024-04-15 14:01:36 +0200

    small changes

--- randomforest.py
+++ randomforest.py
@@ -5,10 +5,10 @@ from matplotlib import pyplot as plt
 def importData(set):
     Header = ["unit number","time, in cycles", "operational setting 1", "operational setting 2", "operational setting 3",
                "sensor measurement 1", "sensor measurement 2", "sensor measurement 3", "sensor measurement 4",
-                 "sensor measurement 5","sensor measurement 6", "sensor measurement 7", "sensor measurement 8", "sensor measurement 9",
+                "sensor measurement 5","sensor measurement 6", "sensor measurement 7", "sensor measurement 8", "sensor measurement 9",
                 "sensor measurement 10", "sensor measurement 11", "sensor measurement 12", "sensor measurement 13",
                 "sensor measurement 14", "sensor measurement 15", "sensor measurement 16", "sensor measurement 17",
-                "sensor measurement 18", "sensor measurement 19", "sensor measurement 20", "sensor measurement 21","sensor measurement 22","sensor measurement 23"]
+                "sensor measurement 18", "sensor measurement 19", "sensor measurement 20", "sensor measurement 21"]
     df = pd.read_csv("Data/{}" .format(set), header=None, delimiter=" ")
     df.columns = Header
     return df
@@ -22,8 +22,9 @@ def add_RUL_column(df):
     return merged
 
 df = importData("train_FD003.txt")
+print(df.isna().sum())
 train=add_RUL_column(df)
-constants=['operational setting 3', 'sensor measurement 1', 'sensor measurement 5', 'sensor measurement 16', 'sensor measurement 18', 'sensor measurement 19', 'sensor measurement 22', 'sensor measurement 23']
+constants=['operational setting 3', 'sensor measurement 1', 'sensor measurement 5', 'sensor measurement 16', 'sensor measurement 18', 'sensor measurement 19']
 #constants have been found using an initial correlation matrix
 train=train.drop(constants, axis=1)
 
