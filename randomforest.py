e2825e08e62cbf4a82c794fc6b8f69819ba9436b
Ida Bundgaard <idbun20@student.sdu.dk>
2024-05-11 13:26:01 +0200

    Removing preprocessing

--- randomforest.py
+++ randomforest.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn import tree
 
 def importData(set):
     Header = ["unit number","time, in cycles", "operational setting 1", "operational setting 2", "operational setting 3",
@@ -13,69 +18,23 @@ def importData(set):
     df.columns = Header
     return df
 
-def add_RUL_column(df):
-    train_grouped_by_unit = df.groupby(by='unit number') 
-    max_time = train_grouped_by_unit['time, in cycles'].max()
+def get_RUL_column(df):
+    grouped_by_unit = df.groupby(by='unit number') 
+    max_time = grouped_by_unit['time, in cycles'].max()
     merged = df.merge(max_time.to_frame(name='max_time'), left_on='unit number',right_index=True)
-    merged["RUL"] = merged["max_time"] - merged['time, in cycles']
-    merged = merged.drop("max_time", axis=1)
-    return merged
+    RUL = merged["max_time"] - merged['time, in cycles']
+    return RUL
 
 df = importData("train_FD003.txt")
-print(df.isna().sum())
-train=add_RUL_column(df)
-constants=['operational setting 3', 'sensor measurement 1', 'sensor measurement 5', 'sensor measurement 16', 'sensor measurement 18', 'sensor measurement 19']
-#constants have been found using an initial correlation matrix
-train=train.drop(constants, axis=1)
+RUL=get_RUL_column(df)
+remaining = ['time, in cycles', 'sensor measurement 2', 'sensor measurement 3',
+       'sensor measurement 4', 'sensor measurement 6', 'sensor measurement 10',
+       'sensor measurement 11', 'sensor measurement 12',
+       'sensor measurement 17'] #taken from preprocessing
 
-corr = train.corr()
+df = df[remaining]
 
-relation_to_RUL=corr.iloc[:,-1] #correlation to target
-print(relation_to_RUL)
-
-to_drop = []
-for index, value in relation_to_RUL.items():
-    if abs(value) < 0.1:
-        to_drop.append(index)
-
-train=train.drop(to_drop, axis=1)
-
-corr=train.corr()
-print(corr.iloc[:,-1])
-
-#fig=plt.figure(figsize=(10,5))
-#ax=fig.add_subplot(111)
-#cax = ax.matshow(corr, vmin=-1, vmax=1)
-#fig.colorbar(cax)
-#ax.set_xticks(np.arange(15))
-#ax.set_xticklabels(train.columns, rotation=90, fontsize=15)
-#ax.set_yticks(np.arange(15))
-#ax.set_yticklabels(train.columns, fontsize=15)
-#plt.show()
-
-#by examination some seem to have a too close correlation, so we will drop some of them
-
-high_corr_indices = []
-for i in range(len(corr)):
-    for j in range(i+1, len(corr)):
-        if abs(corr.iloc[i, j]) > 0.9:
-            high_corr_indices.append((i, j))
-            
-print(high_corr_indices)
-#code above shows for which values the correlation is too high
-too_much_corr = [train.columns[5], train.columns[11], train.columns[12]] #dropped the ones with the lowest correlation to RUL
-train=train.drop(too_much_corr, axis=1)
-
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import cross_val_score
-from sklearn import tree
-
-target_set = train.iloc[:,-1]
-data_set = train.iloc[:,1:-1]
-
-X_train, X_test, y_train, y_test=train_test_split(data_set, target_set, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test=train_test_split(df, RUL, test_size=0.2, random_state=42)
 feature_range = range(1, X_train.shape[1])
 n_estimator_range = [10, 50, 100, 250, 300, 350, 400, 430, 450, 470]
 depth_range = range(10, 14)
@@ -90,28 +49,5 @@ grid = GridSearchCV(rf, param_grid, cv=5, n_jobs=-1)
 grid.fit(X_train, y_train)
 scores = pd.DataFrame(grid.cv_results_)
 
-#n_estimators = [10, 50, 100, 250, 300, 350, 400, 430, 450, 470]
-#max_depth = 10
-#scores = scores[scores['param_max_depth'] == max_depth]
-#scores = scores[scores['param_n_estimators'].isin(n_estimators)]
-#plt.plot(scores['param_n_estimators'], scores['mean_test_score'])
-#plt.xlabel('n_estimators')
-#plt.ylabel('Mean Test Score')
-#plt.title('Mean Test Score vs n_estimators (max_depth=10)')
-#plt.show()
-
 print(grid.best_params_)
 print(grid.best_score_)
-
-#best_rf = grid.best_estimator_
-#test_score = best_rf.score(X_test, y_test)
-#print(test_score)
-
-#rf = RandomForestRegressor(random_state=0, n_estimators=350, max_depth=12)
-
-#rf.fit(X_train, y_train)
-
-#cv_score_train = cross_val_score(rf, X_train, y_train, cv=5)
-#cv_score_test = cross_val_score(rf, X_test, y_test, cv=5)
-#print("Train score:", cv_score_train.mean())
-#print("Test score:", cv_score_test.mean())
