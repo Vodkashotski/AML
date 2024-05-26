7c394f62838e8f820b7db7ea96c7d40e74640ecc
Ida Bundgaard <idbun20@student.sdu.dk>
2024-05-26 16:22:09 +0200

    last changes

--- randomforest.py
+++ randomforest.py
@@ -59,7 +59,7 @@ fig.tight_layout()
 plt.show()
 
 remaining = ['time, in cycles', 'sensor measurement 2', 'sensor measurement 3',
-       'sensor measurement 4', 'sensor measurement 6', 'sensor measurement 10', 'sensor measurement 11', 'sensor measurement 12', 'sensor measurement 13', 'sensor measurement 14',
+       'sensor measurement 4', 'sensor measurement 6', 'sensor measurement 10', 'sensor measurement 11', 'sensor measurement 12',
        'sensor measurement 17'] #taken from preprocessing
 
 train = train[remaining]
@@ -102,10 +102,10 @@ depth_range = np.arange(1, 15)
 
 
 # Saving the data to avoid having to run the loop every time
-# np.save("cross_val_scores_d.npy", scores_cross)
-# np.save("cross_val_std_d.npy", std_cross)
-# np.save("train_std_d.npy", std_train)
-# np.save("scores_train_d.npy", scores_train)
+# np.save("RF_data/cross_val_scores_d.npy", scores_cross)
+# np.save("RF_data/cross_val_std_d.npy", std_cross)
+# np.save("RF_data/train_std_d.npy", std_train)
+# np.save("RF_data/scores_train_d.npy", scores_train)
 
 # scores_cross = []
 # std_cross = []
@@ -122,11 +122,10 @@ depth_range = np.arange(1, 15)
 #             std_train.append(np.array(score['train_score']).std())
 
 # #Saving the data to avoid having to run the loop every time
-# np.save("cross_val_scores.npy", scores_cross)
-# np.save("cross_val_std.npy", std_cross)
-# np.save("train_std.npy", std_train)
-# np.save("scores_test.npy", scores_test)
-# np.save("scores_train.npy", scores_train)
+# np.save("RF_data/cross_val_scores.npy", scores_cross)
+# np.save("RF_data/cross_val_std.npy", std_cross)
+# np.save("RF_data/train_std.npy", std_train)
+# np.save("RF_data/scores_train.npy", scores_train)
 
 # scores_cross = []
 # std_cross = []
@@ -143,10 +142,10 @@ depth_range = np.arange(1, 15)
 #            std_train.append(np.array(score['train_score']).std())
           
 
-# np.save("cross_val_scores_n.npy", scores_cross)
-# np.save("cross_val_std_n.npy", std_cross)
-# np.save("train_std_n.npy", std_train)
-# np.save("scores_train_n.npy", scores_train)
+# np.save("RF_data/cross_val_scores_n.npy", scores_cross)
+# np.save("RF_data/cross_val_std_n.npy", std_cross)
+# np.save("RF_data/train_std_n.npy", std_train)
+# np.save("RF_data/scores_train_n.npy", scores_train)
 
 # X_train, X_test, y_train, y_test=train_test_split(train, RUL, test_size=0.2, random_state=42)
 rf = RandomForestRegressor(random_state=42)
@@ -185,52 +184,52 @@ print("Tuned train RSME score: ", RSME_train)
 print("Tuned test R2 score: ",r2_test)
 print("Tuned test RSME score: ", RSME_test)
 
-# #loading data for plotting
-# score_cross = np.load("cross_val_scores.npy", allow_pickle=True)
-# std_cross = np.load("cross_val_std.npy", allow_pickle=True)
-# std_train = np.load("train_std.npy", allow_pickle=True)
-# scores_train = np.load("scores_train.npy", allow_pickle=True)
+#loading data for plotting
+score_cross = np.load("RF_data/cross_val_scores.npy", allow_pickle=True)
+std_cross = np.load("RF_data/cross_val_std.npy", allow_pickle=True)
+std_train = np.load("RF_data/train_std.npy", allow_pickle=True)
+scores_train = np.load("RF_data/scores_train.npy", allow_pickle=True)
 
-# score_cross_n = np.load("cross_val_scores_n.npy", allow_pickle=True)
-# std_cross_n = np.load("cross_val_std_n.npy", allow_pickle=True)
-# std_train_n = np.load("train_std_n.npy", allow_pickle=True)
-# scores_train_n = np.load("scores_train_n.npy", allow_pickle=True)
+score_cross_n = np.load("RF_data/cross_val_scores_n.npy", allow_pickle=True)
+std_cross_n = np.load("RF_data/cross_val_std_n.npy", allow_pickle=True)
+std_train_n = np.load("RF_data/train_std_n.npy", allow_pickle=True)
+scores_train_n = np.load("RF_data/scores_train_n.npy", allow_pickle=True)
 
-# score_cross_d = np.load("cross_val_scores_d.npy", allow_pickle=True)
-# std_cross_d = np.load("cross_val_std_d.npy", allow_pickle=True)
-# std_train_d = np.load("train_std_d.npy", allow_pickle=True)
-# scores_train_d = np.load("scores_train_d.npy", allow_pickle=True)
+score_cross_d = np.load("RF_data/cross_val_scores_d.npy", allow_pickle=True)
+std_cross_d = np.load("RF_data/cross_val_std_d.npy", allow_pickle=True)
+std_train_d = np.load("RF_data/train_std_d.npy", allow_pickle=True)
+scores_train_d = np.load("RF_data/scores_train_d.npy", allow_pickle=True)
 
-# #plotting
-# fig, ax = plt.subplots(1,3, figsize=(12,4))
-# ax[0].errorbar(feature_range, score_cross, yerr=std_cross, label = 'Cross validation score')
-# ax[0].errorbar(feature_range, scores_train, yerr=std_train, label = 'Train score')
-# ax[0].set_xticks(feature_range)
-# ax[0].set_xticklabels(feature_range, fontsize=12)
-# ax[0].set_yticks([0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9])
-# ax[0].set_yticklabels([0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90],fontsize=12)
-# ax[0].set_xlabel('Max Features',fontsize=14)
-# ax[0].set_ylabel('R2 score',fontsize=14)
+#plotting
+fig, ax = plt.subplots(1,3, figsize=(12,4))
+ax[0].errorbar(feature_range, score_cross, yerr=std_cross, label = 'Cross validation score')
+ax[0].errorbar(feature_range, scores_train, yerr=std_train, label = 'Train score')
+ax[0].set_xticks(feature_range)
+ax[0].set_xticklabels(feature_range, fontsize=12)
+ax[0].set_yticks([0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9])
+ax[0].set_yticklabels([0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90],fontsize=12)
+ax[0].set_xlabel('Max Features',fontsize=14)
+ax[0].set_ylabel('R2 score',fontsize=14)
 
-# ax[1].errorbar(n_estimator_range, score_cross_n, yerr=std_cross_n, label = 'Cross validation score')
-# ax[1].errorbar(n_estimator_range, scores_train_n, yerr=std_train_n, label = 'Train score')
-# ax[1].set_yticks([0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85])
-# ax[1].set_yticklabels([0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85],fontsize=12)
-# ax[1].set_xticks([0, 100, 200, 300, 400, 500, 600])
-# ax[1].set_xticklabels([0, 100, 200, 300, 400, 500, 600], fontsize=12)
-# ax[1].set_xlabel('N estimators',fontsize=14)
-# ax[1].set_ylabel('R2 score',fontsize=14)
+ax[1].errorbar(n_estimator_range, score_cross_n, yerr=std_cross_n, label = 'Cross validation score')
+ax[1].errorbar(n_estimator_range, scores_train_n, yerr=std_train_n, label = 'Train score')
+ax[1].set_yticks([0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85])
+ax[1].set_yticklabels([0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85],fontsize=12)
+ax[1].set_xticks([0, 100, 200, 300, 400, 500, 600])
+ax[1].set_xticklabels([0, 100, 200, 300, 400, 500, 600], fontsize=12)
+ax[1].set_xlabel('N estimators',fontsize=14)
+ax[1].set_ylabel('R2 score',fontsize=14)
 
-# ax[2].errorbar(depth_range, score_cross_d, yerr=std_cross_d, label = 'Cross validation score')
-# ax[2].errorbar(depth_range, scores_train_d, yerr=std_train_d, label = 'Train score')
-# ax[2].set_xticks([2, 4, 6, 8,10, 12 ,14])
-# ax[2].set_xticklabels([2, 4, 6, 8,10, 12 ,14], fontsize=12)
-# ax[2].set_yticks([0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
-# ax[2].set_yticklabels([0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],fontsize=12)
-# ax[2].set_xlabel('Max depth', fontsize=14)
-# ax[2].set_ylabel('R2 score', fontsize=14)
+ax[2].errorbar(depth_range, score_cross_d, yerr=std_cross_d, label = 'Cross validation score')
+ax[2].errorbar(depth_range, scores_train_d, yerr=std_train_d, label = 'Train score')
+ax[2].set_xticks([2, 4, 6, 8,10, 12 ,14])
+ax[2].set_xticklabels([2, 4, 6, 8,10, 12 ,14], fontsize=12)
+ax[2].set_yticks([0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+ax[2].set_yticklabels([0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],fontsize=12)
+ax[2].set_xlabel('Max depth', fontsize=14)
+ax[2].set_ylabel('R2 score', fontsize=14)
 
-# plt.legend(fontsize=12, loc=4)
-# fig.suptitle('Optimisation parameters for Random Forest ', fontsize=16)
-# plt.tight_layout()
-# plt.show()
+plt.legend(fontsize=12, loc=4)
+fig.suptitle('Optimisation parameters for Random Forest ', fontsize=16)
+plt.tight_layout()
+plt.show()
